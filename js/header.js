da8c40e5410ddd8dc804def65fea46578efc317b
german <gerbuscetti@gmail.com>
2024-07-13 16:18:25 -0300

    correcion scss y imagenes home

--- js/header.js
+++ js/header.js
@@ -3,8 +3,62 @@ const menuOpen = doc.querySelector(".menu");
 const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
 const producthome = document.getElementsByClassName("photoproduct");
+
 const imagen = document.getElementById('imagenhome');
 const boton = document.getElementById('buynowbutton');
+const remeras = [
+  "img/imgropa/remeras/remera1.png",
+  "img/imgropa/remeras/remera2.png",
+  "img/imgropa/remeras/remera3.png",
+  "img/imgropa/remeras/remera4.png",
+  "img/imgropa/remeras/remera5.png",
+  "img/imgropa/remeras/remera6.png",
+  "img/imgropa/remeras/remera7.png",
+  "img/imgropa/remeras/remera8.png",
+  "img/imgropa/remeras/remera9.png",
+  "img/imgropa/remeras/remera10.png",
+  "img/imgropa/remeras/remera11.png",
+  "img/imgropa/remeras/remera12.png",
+  "img/imgropa/remeras/remera13.png",
+  "img/imgropa/remeras/remera14.png",
+  "img/imgropa/remeras/remera15.png",
+]
+const zapatillas = [
+  "img/imgropa/zapatillas/zapatilla1.png",
+  "img/imgropa/zapatillas/zapatilla2.png",
+  "img/imgropa/zapatillas/zapatilla3.png",
+  "img/imgropa/zapatillas/zapatilla4.png",
+  "img/imgropa/zapatillas/zapatilla5.png",
+  "img/imgropa/zapatillas/zapatilla6.png",
+  "img/imgropa/zapatillas/zapatilla7.png",
+  "img/imgropa/zapatillas/zapatilla8.png",
+  "img/imgropa/zapatillas/zapatilla9.png",
+  "img/imgropa/zapatillas/zapatilla10.png",
+  "img/imgropa/zapatillas/zapatilla11.png",
+  "img/imgropa/zapatillas/zapatilla12.png",
+  "img/imgropa/zapatillas/zapatilla13.png",
+  "img/imgropa/zapatillas/zapatilla14.png",
+  "img/imgropa/zapatillas/zapatilla15.png",
+]
+const pantalones = [
+  "img/imgropa/pantalones/pantalon1.png",
+  "img/imgropa/pantalones/pantalon2.png",
+  "img/imgropa/pantalones/pantalon3.png",
+  "img/imgropa/pantalones/pantalon4.png",
+  "img/imgropa/pantalones/pantalon5.png",
+  "img/imgropa/pantalones/pantalon6.png",
+  "img/imgropa/pantalones/pantalon7.png",
+  "img/imgropa/pantalones/pantalon8.png",
+  "img/imgropa/pantalones/pantalon9.png",
+  "img/imgropa/pantalones/pantalon10.png",
+  "img/imgropa/pantalones/pantalon11.png",
+  "img/imgropa/pantalones/pantalon12.png",
+  "img/imgropa/pantalones/pantalon13.png",
+  "img/imgropa/pantalones/pantalon14.png",
+  "img/imgropa/pantalones/pantalon15.png",
+]
+
+
 
 
 menuOpen.addEventListener("click", () => {
@@ -16,65 +70,16 @@ menuClose.addEventListener("click", () => {
 });
 
 
-const remeras = [
-  "img/imgropa/remeras/remera1.jpg",
-  "img/imgropa/remeras/remera2.jpg",
-  "img/imgropa/remeras/remera3.jpg",
-  "img/imgropa/remeras/remera4.jpg",
-  "img/imgropa/remeras/remera5.jpg",
-  "img/imgropa/remeras/remera6.jpg",
-  "img/imgropa/remeras/remera7.jpg",
-  "img/imgropa/remeras/remera8.jpg",
-  "img/imgropa/remeras/remera9.jpg",
-  "img/imgropa/remeras/remera10.jpg",
-  "img/imgropa/remeras/remera11.jpg",
-  "img/imgropa/remeras/remera12.jpg",
-  "img/imgropa/remeras/remera13.jpg",
-  "img/imgropa/remeras/remera14.jpg",
-  "img/imgropa/remeras/remera15.jpg",
-]
-const zapatillas = [
-    "img/imgropa/zapatillas/zapatilla1.jpg",
-    "img/imgropa/zapatillas/zapatilla2.jpg",
-    "img/imgropa/zapatillas/zapatilla3.jpg",
-    "img/imgropa/zapatillas/zapatilla4.jpg",
-    "img/imgropa/zapatillas/zapatilla5.jpg",
-    "img/imgropa/zapatillas/zapatilla6.jpg",
-    "img/imgropa/zapatillas/zapatilla7.jpg",
-    "img/imgropa/zapatillas/zapatilla8.jpg",
-    "img/imgropa/zapatillas/zapatilla9.jpg",
-    "img/imgropa/zapatillas/zapatilla10.jpg",
-    "img/imgropa/zapatillas/zapatilla11.jpg",
-    "img/imgropa/zapatillas/zapatilla12.jpg",
-    "img/imgropa/zapatillas/zapatilla13.jpg",
-    "img/imgropa/zapatillas/zapatilla14.jpg",
-    "img/imgropa/zapatillas/zapatilla15.jpg",
-  ]
-  const pantalones = [
-    "img/imgropa/pantalones/pantalon1.jpg",
-    "img/imgropa/pantalones/pantalon2.jpg",
-    "img/imgropa/pantalones/pantalon3.jpg",
-    "img/imgropa/pantalones/pantalon4.jpg",
-    "img/imgropa/pantalones/pantalon5.jpg",
-    "img/imgropa/pantalones/pantalon6.jpg",
-    "img/imgropa/pantalones/pantalon7.jpg",
-    "img/imgropa/pantalones/pantalon8.jpg",
-    "img/imgropa/pantalones/pantalon9.jpg",
-    "img/imgropa/pantalones/pantalon10.jpg",
-    "img/imgropa/pantalones/pantalon11.jpg",
-    "img/imgropa/pantalones/pantalon12.jpg",
-    "img/imgropa/pantalones/pantalon13.jpg",
-    "img/imgropa/pantalones/pantalon14.jpg",
-    "img/imgropa/pantalones/pantalon15.jpg",
-  ]
-
 function mostrar_remeras() {
-  
+
+
+
+  // Recorre todos los elementos y cambia la imagen
   for (var i = 0; i < producthome.length; i++) {
 
     var productdata = producthome[i].parentNode.querySelector('p');
-
-    producthome[i].src = remeras[i]; 
+    
+    producthome[i].src = remeras[i]; // Reemplaza "img/nueva-imagen.jpg" con la ruta de la nueva imagen que deseas usar
     productdata.textContent = "Remera";
   }
 }
@@ -99,7 +104,7 @@ function mostrar_zapatillas() {
       }
 }
 function ir_home() {
-  window.location.href = "home.html";
+  window.location.href = "index.html";
 }
 
 function mostrar_ropa() {
